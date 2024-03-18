dbada7eea7a2bbcb1fdc6d8e90d848d01370babe
Tymec <tymek1rt@hotmail.com>
2024-03-18 22:26:40 +0100

    Add loss abstract class

--- nn/loss.py
+++ nn/loss.py
@@ -1,7 +1,19 @@
 """Loss functions"""
 
+from abc import ABC, abstractmethod
+
 import numpy as np
 import numpy.typing as npt
+
+
+class Loss(ABC):
+    @staticmethod
+    @abstractmethod
+    def f(y_pred: npt.ArrayLike, y_true: npt.ArrayLike) -> npt.ArrayLike: ...
+
+    @staticmethod
+    @abstractmethod
+    def df(y_pred: npt.ArrayLike, y_true: npt.ArrayLike) -> npt.ArrayLike: ...
 
 
 class MeanSquaredError:
@@ -12,3 +24,28 @@ class MeanSquaredError:
     @staticmethod
     def df(y_pred: npt.ArrayLike, y_true: npt.ArrayLike) -> npt.ArrayLike:
         return y_pred - y_true
+
+
+"""
+    MSELoss
+L1Loss
+CrossEntropyLoss
+CTCLoss
+NLLLoss
+PoissonNLLLoss
+GaussianNLLLoss
+KLDivLoss
+BCELoss
+BCEWithLogitsLoss
+MarginRankingLoss
+HingeEmbeddingLoss
+MultiLabelMarginLoss
+HuberLoss
+SmoothL1Loss
+SoftMarginLoss
+MultiLabelSoftMarginLoss
+CosineEmbeddingLoss
+MultiMarginLoss
+TripletMarginLoss
+TripletMarginWithDistanceLoss
+"""
